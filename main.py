0a096595838a6dd162f93aa0eca29bc5fc57a73f
Andres Alleva <andres.alleva@gmail.com>
2026-06-23 23:06:23 +0100

    Done with ReAct implementation in Langgraph

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 AGENT_REASON = "agent_reason"
 ACT = "act"
-LAST = "-1"
+LAST = -1
 
 def should_continue(state: MessagesState) -> str:
     if not state["messages"][LAST].tool_calls:
@@ -29,7 +29,25 @@ app = flow.compile()
 app.get_graph().draw_mermaid_png(output_file_path="flow.png")
 
 def main():
-    print("Hello ReAct LangGraph with Function Calling")
+    print("Hello ReAct LangGraph with Function Calling\n")
+
+    # Test 1: Math tool (triple)
+    # print("Test 1: Triple of 7")
+    # res = app.invoke({"messages": [HumanMessage(content="What is the triple of 7?")]})
+    #print(res["messages"][LAST].content)
+    #print()
+
+    # Test 2: Web search tool (TavilySearch)
+    #print("Test 2: Weather in London")
+    #res = app.invoke({"messages": [HumanMessage(content="What is the weather today in London?")]})
+    #print(res["messages"][LAST].content)
+    #print()
+
+    print("Test 3: Weather is London and triple it")
+    res = app.invoke({"messages": [HumanMessage(content="What is the celsius temperature today in London? List it and triple it")]})
+    print(res["messages"][LAST].content)
+    print()
+
 
 if __name__ == "__main__":
     main()
